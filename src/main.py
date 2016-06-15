02679bba641cb94b7fb8641001a3fdf45c384ef3
dukemiller <dukemiller928@hotmail.com>
2016-06-14 21:26:55 -0700

    Multithreaded encoding where possible

--- src/main.py
+++ src/main.py
@@ -5,9 +5,9 @@ def main():
     video = Video("https://youtu.be/RHFVIH-TewM")
     path = r"c:\users\duke\downloads"
 
-    if video.is_not_in(r"c:\users\duke\downloads"):
+    if video.is_not_in(path):
         video.download_to(path)\
-            .then_encode()\
+            .then_encode(in_new_thread=True)\
             .and_remove_unencoded_file()
 
 if __name__ == '__main__':
--- src/youtube.py
+++ src/youtube.py
@@ -1,4 +1,6 @@
+from time import sleep
 import subprocess
+import threading
 import tools
 import pafy
 import os
@@ -45,23 +47,37 @@ class DownloadedVideo:
         self.directory = directory
         self.download_path = download_path
 
-    def then_encode(self) -> 'EncodedVideo':
+    def then_encode(self, in_new_thread=False) -> 'EncodedVideo':
         encoded_path = os.path.join(self.directory, self.video.encoded_file_name())
         command = 'ffmpeg -i "{0}" -f {1} "{2}"'.format(self.download_path, self.video.desired_extension, encoded_path)
+
+        if in_new_thread:
+            thread = threading.Thread(target=self._create_process, args=(command,))
+            thread.start()
+            return EncodedVideo(self.download_path, thread)
+
+        self._create_process(command)
+        return EncodedVideo(self.directory)
+
+    @staticmethod
+    def _create_process(command: str):
         process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         process.communicate()
-
-        return EncodedVideo(self, self.directory, self.download_path)
 
 
 class EncodedVideo:
 
-    def __init__(self, video: DownloadedVideo, directory: str, download_path: str):
-        self.video = video
-        self.directory = directory
+    def __init__(self, download_path: str, thread: threading.Thread = None):
         self.download_path = download_path
+        self.thread = thread
 
     def and_remove_unencoded_file(self) -> None:
+        if self.thread is not None:
+            threading.Thread(target=self._sit_and_wait_then_remove).start()
+        else:
+            os.remove(self.download_path)
+
+    def _sit_and_wait_then_remove(self):
+        while self.thread.is_alive():
+            sleep(0.2)
         os.remove(self.download_path)
-
-
