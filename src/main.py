34b9a3ed964ac78ec862aaefc38d1f6115796d75
dukemiller <dukemiller928@hotmail.com>
2016-06-14 20:59:54 -0700

    Most logic about downloading a single video added I did it in a sort of object guided building / method chaining pattern because it seemed fun.

--- src/main.py
+++ src/main.py
@@ -1,6 +1,14 @@
-def main():
-    pass
+from youtube import Video
 
+
+def main():
+    video = Video("https://youtu.be/RHFVIH-TewM")
+    path = r"c:\users\duke\downloads"
+
+    if video.is_not_in(r"c:\users\duke\downloads"):
+        video.download_to(path)\
+            .then_encode()\
+            .and_remove_unencoded_file()
 
 if __name__ == '__main__':
-    pass
+    main()
--- src/youtube.py
+++ src/youtube.py
@@ -1,3 +1,4 @@
+import subprocess
 import tools
 import pafy
 import os
@@ -22,25 +23,45 @@ class Video:
     def encoded_file_name(self) -> str:
         return tools.format_filename(self._filename.format(self.title, self.desired_extension))
 
-    def is_already_in(self, path) -> bool:
-        return self.encoded_file_name() in os.listdir(path)
+    def is_not_in(self, path) -> bool:
+        return self.encoded_file_name() not in os.listdir(path)
 
     def _details(self) -> str:
         return '"{0}" ===== [{1}] @ {2} (around ~{3} MB)'.format(self.title, self.pafy_object.duration, self.stream.bitrate, self.size())
 
-    def download_to(self, path):
+    def download_to(self, directory) -> 'DownloadedVideo':
         print("Downloading {0}".format(self._details()))
-        download_path = os.path.join(path, self.file_name())
+
+        download_path = os.path.join(directory, self.file_name())
         self.stream.download(filepath=download_path, quiet=True)
 
-        return DownloadedVideo(self)
+        return DownloadedVideo(self, directory, download_path)
 
 
-class DownloadedVideo(Video):
+class DownloadedVideo:
 
-    def then_encode(self):
-        pass
+    def __init__(self, video: Video, directory: str, download_path: str):
+        self.video = video
+        self.directory = directory
+        self.download_path = download_path
+
+    def then_encode(self) -> 'EncodedVideo':
+        encoded_path = os.path.join(self.directory, self.video.encoded_file_name())
+        command = 'ffmpeg -i "{0}" -f {1} "{2}"'.format(self.download_path, self.video.desired_extension, encoded_path)
+        process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        process.communicate()
+
+        return EncodedVideo(self, self.directory, self.download_path)
 
 
-video = Video("https://youtu.be/RHFVIH-TewM")
-video.download_to("c:\\").then_encode()
+class EncodedVideo:
+
+    def __init__(self, video: DownloadedVideo, directory: str, download_path: str):
+        self.video = video
+        self.directory = directory
+        self.download_path = download_path
+
+    def and_remove_unencoded_file(self) -> None:
+        os.remove(self.download_path)
+
+
